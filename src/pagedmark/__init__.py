a9982d27aafe4dea39bfd18cc778b5375e8c5127
doofzoff <doofzoff@users.noreply.github.com>
2026-08-19 22:04:51 +0400

    release 0.1.4

--- src/pagedmark/__init__.py
+++ src/pagedmark/__init__.py
@@ -32,7 +32,7 @@ _os.environ.setdefault("TRANSFORMERS_VERBOSITY", "error")
 _warnings.filterwarnings("ignore", message=r".*ImageProcessorFast.*")
 
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 
 __all__ = [
     "BatchSummary",
